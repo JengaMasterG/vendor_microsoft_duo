95c1ae4e98d0799968bf23d9aff4e7f563d3d2b5
JengaMasterG <grsruggirello25@gmail.com>
2022-05-31 20:57:07 -0400

    Vendor Tree after Script

--- bin/init.surface.touch.preloads.sh
+++
@@ -1,3 +0,0 @@
-#! /vendor/bin/sh
-cp /vendor/sfpd/display/PixelAlignmentData.bin /data/vendor/vendor.touchpen_hal/TwoPanelAlignment.bin
-chown system.system /data/vendor/vendor.touchpen_hal/TwoPanelAlignment.bin
